26882d64e016f9b90a27c6a9186171fa1364c5a0
Divya Darshan <parukrish.darshan20@gmail.com>
2025-01-15 18:20:15 +0530

    upload chainsaw man

--- anime/chain/main.js
+++ anime/chain/main.js
@@ -1,16 +1,19 @@
 
 var vid = document.getElementById('vid');
 
-function gbd1(){
-    vid.src = 'https://geo.dailymotion.com/player.html?video=x96d3z8';
+function ch1(){
+    vid.src = 'https://player.vimeo.com/video/1047084442?h=9b6ffa92b7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479';
 }
 
-function gbd2(){
-    vid.src = 'https://geo.dailymotion.com/player.html?video=x96d1bc';
+function ch2(){
+    vid.src = 'https://player.vimeo.com/video/1047084442?h=9b6ffa92b7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479';
 }
 
-function gbd3(){
+function ch3(){
     vid.src = 'https://geo.dailymotion.com/player.html?video=x96d3z6';
 }
 
+function ch5(){
+    vid.src = 'https://player.vimeo.com/video/1047078084?h=b9c15311ae&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479';
+}
 
